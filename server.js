a2b6379e93f5856a21be1bf964a011c4c1004035
reski-id <pesan.reski@gmail.com>
2022-11-08 21:57:11 +0700

    SimpleServer

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+const todos=[
+    { id :1,text:'todo one'},
+    { id :2,text:'todo two'},
+    { id :3,text:'todo three'},
+];
+
+const server = http.createServer((req, res)=>{
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('X-poweredBy', 'NodeJS');
+  
+    res.end(
+        JSON.stringify({
+        success:true,
+        data:todos
+    }));
+
+});
+
+const PORT = 5000;
+
+server.listen(PORT, ()=>console.log(`Server running on port ${PORT}`));
